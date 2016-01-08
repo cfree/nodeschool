8cb12ae394a16650af136ac40efdc687f33c4a14
Craig Freeman <craigfreeman@gmail.com>
2016-01-07 18:56:15 -0700

    Program 8

--- learnyounode/program8.js
+++ learnyounode/program8.js
@@ -1,8 +1,14 @@
-var http = require('http');
+var http = require('http'),
+	BufferList = require('bl'),
+	bl = new BufferList(),
+	results;
 
 http.get(process.argv[2], function(res) {
 	res.setEncoding('utf8');
-	res.on('data', function(data) {
-		console.log(data);
-	})
+	
+	res.pipe(BufferList(function(err, data) {
+		results = data.toString();
+		console.log(results.length);
+		console.log(results);
+	}));
 });
